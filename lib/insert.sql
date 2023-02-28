0b27939dafc767851c088731a93bc75930d83525
George Elkik <gelkik@gmail.com>
2023-02-27 19:11:31 -0500

    final

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Mr. Chocolate', 35, 'M', 'brown', 'no', True 
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Rowdy', 30, 'M', 'blue', 'yes', False 
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Tabitha', 40, 'F', 'red', 'yes', False 
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Sergeant Brown', 45, 'M', 'black', 'yes', True 
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Melissa', 25, 'F', 'blue', 'no', True 
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Grinch', 55, 'M', 'green', 'yes', True 
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Wendy', 20, 'F', 'orange', 'no', True 
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    NULL, 25, 'F', 'orange', 'yes', True 
+);
